c114d7ebddafaa6ad015820ebc820f53850af750
heungall <gibbum93@gmail.com>
2022-06-03 22:00:00 +0900

    test KoMoRan

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
+from traceback import print_list
 from PyKomoran import Komoran, DEFAULT_MODEL
 
 komoran = Komoran(DEFAULT_MODEL['LIGHT'])
-komoran.get_list('이것은 테스트 문장입니다')
+print(komoran.get_list('이것은 테스트 문장입니다'))
+print(komoran.get_list('SAP 계정이 잠겼습니다'))
+print(komoran.get_list('웹ERP 계정이 잠겼습니다'))
+
